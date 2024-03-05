b1bf7afcb2e5de345a4f4f94b7cc46eb70c3a2ee
Muhammad Rizky <rizky.wazza@gmail.com>
2024-03-05 15:39:04 +0100

    tasks done

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
   # root "posts#index"
+  get "/tasks/:id", to: "tasks#show", as: :task
+  delete "/tasks/:id", to: "tasks#destroy"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_103919) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
